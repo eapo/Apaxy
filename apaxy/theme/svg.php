4729790eda7df6e17406e3812bbe578a58144691
eapo <e@eaposztrof.com>
2023-02-05 23:55:32 +0100

    fixing FontAwesome sprite path

--- apaxy/theme/svg.php
+++ apaxy/theme/svg.php
@@ -5,7 +5,7 @@
   function svg($q){
     $default = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"></path></svg>';
     $query = (!empty($q))?$q:'file-o';
-    $xml = simplexml_load_file("icons/solid.svg") or die($default);
+    $xml = simplexml_load_file("solid.svg") or die($default);
     foreach($xml->symbol as $symbol) {
       if ($symbol['id'] == $query) {
         $viewBox = $symbol['viewBox'];
